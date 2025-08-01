6ce0a7eb9683402ef385d6ff7c806faee251fd3e
abdullahmeen <abdullahamen321@gmail.com>
2025-08-01 16:20:57 +0500

    made some adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -13,43 +13,43 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: UTip(),
+      home: const UTip(),
     );
   }
 }
 
-class UTip extends StatelessWidget {
+class UTip extends StatefulWidget {
   const UTip({super.key});
 
   @override
+  State<UTip> createState() => _UTipState();
+}
+
+class _UTipState extends State<UTip> {
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text('UTip'),
-      ),
+    var theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('UTip')),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
             padding: const EdgeInsets.all(08),
             decoration: BoxDecoration(
-              color: Colors.deepPurple[400],
-              borderRadius: BorderRadius.circular(8)
+              color: theme.colorScheme.inversePrimary,
+              borderRadius: BorderRadius.circular(18),
               //buildthemecontext
             ),
             child: Column(
-
               children: [
-                const Text('Per Person',
-                style: TextStyle(
-                  fontSize: 28,
-                ),),
-                const Text('\$29.9'),
+                Text('Total Per Person', style: theme.textTheme.titleMedium),
+                Text('\$29.9', style: theme.textTheme.displaySmall),
               ],
-            )),
+            ),
+          ),
         ],
       ),
     );
